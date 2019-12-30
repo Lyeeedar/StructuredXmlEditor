5b52ef3c404ae7fe16ce1675c874b572548132fc
lyeeedar <lyeeedar@gmail.com>
2019-12-30 11:33:25 +0000

    Begin work integrating the git app in

--- StructuredXmlEditor/Data/Workspace.cs
+++ StructuredXmlEditor/Data/Workspace.cs
@@ -237,6 +237,7 @@ namespace StructuredXmlEditor.Data
 			Tools.Add(new ProjectViewTool(this));
 			Tools.Add(new TemplateCreatorTool(this));
 			Tools.Add(new FocusTool(this));
+			Tools.Add(new GitTool(this));
 
 			Thread workerThread = new Thread(WorkerThreadLoop);
 			workerThread.IsBackground = true;
---
+++ StructuredXmlEditor/Tools/GitTool.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GitApp;
+using StructuredXmlEditor.Data;
+
+namespace StructuredXmlEditor.Tools
+{
+	public class GitTool : ToolBase
+	{
+		public ViewModelView View
+		{
+			get
+			{
+				if (m_view == null)
+				{
+					m_view = new GitApp.ViewModelView();
+				}
+
+				return m_view;
+			}
+		}
+		private GitApp.ViewModelView m_view;
+
+		public GitTool(Workspace workspace) : base(workspace, "Git Tool")
+		{
+			DefaultPositionDocument = ToolPosition.Document;
+		}
+	}
+}
