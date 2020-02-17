71ca0c8c23d770c42640b61a08ea43d9f8fc4725
lyeeedar <lyeeedar@gmail.com>
2020-02-17 20:01:50 +0000

    Add a preview for the transform.

--- StructuredXmlEditor/Tools/DataTransformerTool.cs
+++ StructuredXmlEditor/Tools/DataTransformerTool.cs
@@ -9,6 +9,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 using System.Xml.Linq;
 
 namespace StructuredXmlEditor.Tools
@@ -74,7 +75,15 @@ namespace StructuredXmlEditor.Tools
 		public SideBySideDiffModel DiffModel { get; set; }
 
 		//-----------------------------------------------------------------------
+		public TransformPreview Preview { get; set; }
+
+		//-----------------------------------------------------------------------
 		public Command<object> DoTransformCMD { get { return new Command<object>((e) => DoTransform()); } }
+
+		//-----------------------------------------------------------------------
+		public Command<object> ReturnCMD { get { return new Command<object>((e) => Return()); } }
+		public Command<object> SaveCMD { get { return new Command<object>((e) => Save()); } }
+		public Command<object> SaveAllCMD { get { return new Command<object>((e) => SaveAll()); } }
 
 		//-----------------------------------------------------------------------
 		public DataTransformerTool(Workspace workspace) : base(workspace, "Data Transformer Tool")
@@ -118,7 +127,7 @@ namespace StructuredXmlEditor.Tools
 		//-----------------------------------------------------------------------
 		public void DoTransform()
 		{
-			var transformCount = 0;
+			var preview = new TransformPreview();
 
 			var projectDir = Path.GetDirectoryName(Workspace.ProjectRoot);
 			var files = Directory.EnumerateFiles(projectDir, "*", SearchOption.AllDirectories).ToList();
@@ -130,18 +139,98 @@ namespace StructuredXmlEditor.Tools
 				{
 					try
 					{
-						var success = DataTransformer.TransformDocument(file);
+						var doc = XDocument.Load(file);
+						var transformed = DataTransformer.TransformDocument(doc.Root);
+						var asString = new StringBuilder();
 
-						if (success)
+						XmlWriterSettings settings = new XmlWriterSettings
 						{
-							transformCount++;
+							Indent = true,
+							IndentChars = "\t",
+							NewLineChars = "\r\n",
+							NewLineHandling = NewLineHandling.Replace,
+							OmitXmlDeclaration = true,
+							Encoding = new UTF8Encoding(false)
+						};
+
+						using (XmlWriter writer = XmlTextWriter.Create(asString, settings))
+						{
+							doc.Save(writer);
+						}
+
+						var original = File.ReadAllText(file);
+
+						var builder = new SideBySideDiffBuilder(new Differ());
+						var diff = builder.BuildDiffModel(original, asString.ToString());
+
+						if (transformed)
+						{
+							preview.Files.Add(new Tuple<string, string, string, SideBySideDiffModel>(file, Path.GetFileNameWithoutExtension(file), asString.ToString(), diff));
 						}
 					}
 					catch (Exception) { }
 				}
 			}
 
-			Message.Show($"Transformed {transformCount} files.", "Completed transform");
+			if (preview.Files.Count == 0)
+			{
+				Message.Show("No matching files found in project", "Completed transform");
+			}
+			else
+			{
+				Preview = preview;
+				RaisePropertyChangedEvent(nameof(Preview));
+			}
 		}
+
+		//-----------------------------------------------------------------------
+		public void Return()
+		{
+			Preview = null;
+			RaisePropertyChangedEvent(nameof(Preview));
+		}
+
+		//-----------------------------------------------------------------------
+		public void Save()
+		{
+			var selected = Preview?.Selected;
+
+			if (selected == null) return;
+
+			File.WriteAllText(selected.Item1, selected.Item3);
+			Preview.Files.Remove(selected);
+			Preview.Selected = null;
+
+			if (Preview.Files.Count == 0)
+			{
+				Return();
+			}
+		}
+
+		//-----------------------------------------------------------------------
+		public void SaveAll()
+		{
+			foreach (var file in Preview.Files)
+			{
+				File.WriteAllText(file.Item1, file.Item3);
+			}
+			Return();
+		}
+	}
+
+	//-----------------------------------------------------------------------
+	public class TransformPreview : NotifyPropertyChanged
+	{
+		public DeferableObservableCollection<Tuple<string, string, string, SideBySideDiffModel>> Files { get; } = new DeferableObservableCollection<Tuple<string, string, string, SideBySideDiffModel>>();
+		public Tuple<string, string, string, SideBySideDiffModel> Selected
+		{
+			get { return m_selected; }
+			set
+			{
+				m_selected = value;
+				RaisePropertyChangedEvent();
+			}
+		}
+		private Tuple<string, string, string, SideBySideDiffModel> m_selected;
 	}
 }
