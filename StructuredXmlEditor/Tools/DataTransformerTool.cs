aa6cb0e25537301c8960271676167d67a5647b87
lyeeedar <lyeeedar@gmail.com>
2020-02-19 06:01:32 +0000

    Add xml tag autocomplete

--- StructuredXmlEditor/Tools/DataTransformerTool.cs
+++ StructuredXmlEditor/Tools/DataTransformerTool.cs
@@ -6,11 +6,13 @@ using StructuredXmlEditor.Data;
 using StructuredXmlEditor.View;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Xml;
 using System.Xml.Linq;
@@ -93,8 +95,10 @@ namespace StructuredXmlEditor.Tools
 			TextEditor.Text = "{el||}";
 			TextEditor.Foreground = new SolidColorBrush(Color.FromRgb(255, 255, 255));
 			TextEditor.Background = Brushes.Transparent;
-			TextEditor.Document.TextChanged += (e, args) => 
+			TextEditor.TextArea.TextEntered += (e, args) => 
 			{
+				Autocomplete(e, args);
+
 				UpdatePreview();
 			};
 
@@ -225,6 +229,161 @@ namespace StructuredXmlEditor.Tools
 				File.WriteAllText(file.Item1, file.Item3);
 			}
 			Return();
+		}
+
+		//-----------------------------------------------------------------------
+		public void Autocomplete(object sender, TextCompositionEventArgs e)
+		{
+			if (e.Text == ">")
+			{
+				//auto-insert closing element
+				int offset = TextEditor.CaretOffset;
+				string s = GetElementAtCursor(TextEditor.Text, offset - 1);
+				if (!string.IsNullOrWhiteSpace(s) && "!--" != s)
+				{
+					if (!IsClosingElement(TextEditor.Text, offset - 1, s))
+					{
+						string endElement = "</" + s + ">";
+						var rightOfCursor = TextEditor.Text.Substring(offset, Math.Max(0, Math.Min(endElement.Length + 50, TextEditor.Text.Length) - offset - 1)).TrimStart();
+						if (!rightOfCursor.StartsWith(endElement))
+						{
+							TextEditor.TextArea.Document.Insert(offset, endElement);
+							TextEditor.CaretOffset = offset;
+						}
+					}
+				}
+			}
+			else if (e.Text == "/")
+			{
+				int offset = TextEditor.CaretOffset;
+				if (TextEditor.Text.Length > offset + 2 && TextEditor.Text[offset] == '>')
+				{
+					//remove closing tag if exist
+					string s = GetElementAtCursor(TextEditor.Text, offset - 1);
+					if (!string.IsNullOrWhiteSpace(s))
+					{
+						//search closing end tag. Element must be empty (whitespace allowed)  
+						//"<hallo>  </hallo>" --> enter '/' --> "<hallo/>  "
+						string expectedEndTag = "</" + s + ">";
+						for (int i = offset + 1; i < TextEditor.Text.Length - expectedEndTag.Length + 1; i++)
+						{
+							if (!char.IsWhiteSpace(TextEditor.Text[i]))
+							{
+								if (TextEditor.Text.Substring(i, expectedEndTag.Length) == expectedEndTag)
+								{
+									//remove already existing endTag
+									TextEditor.Document.Remove(i, expectedEndTag.Length);
+								}
+								break;
+							}
+						}
+					}
+				}
+			}
+			else if (e.Text == "{")
+			{
+				int offset = TextEditor.CaretOffset;
+				TextEditor.TextArea.Document.Insert(offset, "el||}");
+				TextEditor.CaretOffset = offset+3;
+			}
+		}
+
+		//-----------------------------------------------------------------------
+		/// <summary>
+		/// Source: https://xsemmel.codeplex.com
+		/// </summary>
+		/// <param name="xml"></param>
+		/// <param name="offset"></param>
+		/// <returns></returns>
+		public static string GetElementAtCursor(string xml, int offset)
+		{
+			if (offset == xml.Length)
+			{
+				offset--;
+			}
+			int startIdx = xml.LastIndexOf('<', offset);
+			if (startIdx < 0) return null;
+
+			if (startIdx < xml.Length && xml[startIdx + 1] == '/')
+			{
+				startIdx = startIdx + 1;
+			}
+
+			int endIdx1 = xml.IndexOf(' ', startIdx);
+			if (endIdx1 == -1 /*|| endIdx1 > offset*/) endIdx1 = int.MaxValue;
+
+			int endIdx2 = xml.IndexOf('>', startIdx);
+			if (endIdx2 == -1 /*|| endIdx2 > offset*/)
+			{
+				endIdx2 = int.MaxValue;
+			}
+			else
+			{
+				if (endIdx2 < xml.Length && xml[endIdx2 - 1] == '/')
+				{
+					endIdx2 = endIdx2 - 1;
+				}
+			}
+
+			int endIdx = Math.Min(endIdx1, endIdx2);
+			if (endIdx2 > 0 && endIdx2 < int.MaxValue && endIdx > startIdx)
+			{
+				return xml.Substring(startIdx + 1, endIdx - startIdx - 1);
+			}
+			else
+			{
+				return null;
+			}
+		}
+
+		//-----------------------------------------------------------------------
+		/// <summary>
+		/// Source: https://xsemmel.codeplex.com
+		/// Liefert true falls das Element beim offset ein schließendes Element ist,
+		/// also &lt;/x&gt; oder &lt;x/&gt;
+		/// </summary>
+		/// <param name="xml"></param>
+		/// <param name="offset"></param>
+		/// <param name="elementName">optional, elementName = GetElementAtCursor(xml, offset)</param>
+		/// <returns></returns>
+		public static bool IsClosingElement(string xml, int offset, string elementName = null)
+		{
+			if (elementName == null)
+			{
+				elementName = GetElementAtCursor(xml, offset);
+			}
+			else
+			{
+				Debug.Assert(GetElementAtCursor(xml, offset) == elementName);
+			}
+
+			if (offset >= xml.Length || offset < 0)
+			{
+				return false;
+			}
+			int idxOpen = xml.LastIndexOf('<', offset);
+			if (idxOpen < 0)
+			{
+				return false;
+			}
+
+			int idxClose = xml.LastIndexOf('>', offset);
+			if (idxClose > 0)
+			{
+				if (idxClose > idxOpen && idxClose < offset - 1)
+				{
+					return false;
+				}
+			}
+
+			string prefix = xml.Substring(idxOpen, offset - idxOpen);
+			if (prefix.Contains("/"))
+			{
+				return true;
+			}
+
+
+			return false;
 		}
 	}
 
